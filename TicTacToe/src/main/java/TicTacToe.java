e8be21675d11778a59031d9e1796d9813da486a9
Sven-Ot <s.ottersberg@megadruck.de>
2020-10-27 13:30:47 +0100

    Update TicTacToe.java

--- TicTacToe/src/main/java/TicTacToe.java
+++ TicTacToe/src/main/java/TicTacToe.java
@@ -8,13 +8,36 @@
  *
  * @author sveno
  */
-public class PrinzipDesSpiels {
+public class TicTacToe {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        public int ermittleGewinner(){
+            int arr[][];
+            for(int i=0;i==3;i++){            
+                if(arr[i][1]==arr[i][2]&&arr[i][1]==arr[i][3]){
+                    return arr[i][1];
+                }
+            }
+            for(int i=0;i==3;i++){            
+                if(arr[1][i]==arr[2][i]&&arr[1][i]==arr[1][i]){
+                    return arr[1][i];
+                }
+            }
+            if(arr[1][1]==arr[2][2]&&arr[1][1]==arr[3][3]){
+                return arr[1][1];
+            }
+            if(arr[1][3]==arr[2][2]&&arr[1][3]==arr[3][1]){
+                return arr[1][1];
+            }
+        }
+        public void setzeIcon(){
+        }
+        public void wechselSpieler(){
+        } 
     }
 
 }
