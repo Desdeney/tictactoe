a19f3d653e9a4679e03b81d817e503b835a5cb4c
Fabian Ernst <fabian.ernst@yousic.net>
2020-10-27 12:31:41 +0100

    gg
    gg

--- TicTacToe/src/main/java/TicTacToe.java
+++ TicTacToe/src/main/java/TicTacToe.java
@@ -8,13 +8,18 @@
  *
  * @author sveno
  */
-public class PrinzipDesSpiels {
+public class TicTacToe {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    }
+
+    public int setzeIcon(){
+        int icon = Spieler.getSymbol(spielernr);
+        return icon;
     }
 
 }
